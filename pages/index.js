5df463afda96ef66197f1ea2879d4aacbbfd2204
Visnu Pitiyanuvath <visnup@gmail.com>
2019-01-16 08:48:56 -0800

    styled components

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,8 @@
-export default () => <div>Set</div>
+import React from 'react'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  color: red;
+`
+
+export default () => <Title>My page</Title>
