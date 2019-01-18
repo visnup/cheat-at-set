9132de533ba618c245e547dd46a866727b58e654
Visnu Pitiyanuvath <visnup@gmail.com>
2019-01-18 11:34:19 -0800

    clean up

--- components/cheat.js
+++ components/cheat.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 import { color } from 'd3-color'
 import { schemeCategory10 } from 'd3-scale-chromatic'
-import DOM from './DOM'
 import cards from './cards'
 import sets from './sets'
 
@@ -18,8 +17,9 @@ function cheat(src, canvas) {
   })
 
   const image = ctx.getImageData(0, 0, canvas.width, canvas.height)
-  sets(cards(image)).forEach((set, i) => {
-    ctx.strokeStyle = colors[i % colors.length]
+  let i = 0
+  for (const set of sets(cards(image))) {
+    ctx.strokeStyle = colors[i++ % colors.length]
     for (const card of set) {
       ctx.lineWidth = (card.width = card.width || 18)
       card.width /= 2
@@ -29,7 +29,7 @@ function cheat(src, canvas) {
       ctx.closePath()
       ctx.stroke()
     }
-  })
+  }
 }
 
 class Cheat extends Component {
@@ -40,11 +40,10 @@ class Cheat extends Component {
     this.video.srcObject = src
 
     this.video.addEventListener('loadeddata', () => {
-      if (this.canvas) {
-        const { width, height } = DOM.size(this.video)
-        this.canvas.width = width
-        this.canvas.height = height
-      }
+      const track = this.video.srcObject.getVideoTracks()[0]
+      const { width, height } = track.getSettings()
+      this.canvas.width = width
+      this.canvas.height = height
     })
 
     this.loop()
@@ -52,17 +51,16 @@ class Cheat extends Component {
 
   loop = () => {
     const { video, canvas } = this
-    if (!canvas) return
-
-    cheat(video, canvas)
-    setTimeout(this.loop, 30)
+    if (video && canvas) {
+      cheat(video, canvas)
+      requestAnimationFrame(this.loop)
+    }
   }
 
   render() {
     return (
       <div {...this.props}>
-        <video ref={ref => this.video = ref} autoPlay muted playsInline src="https://gist.githubusercontent.com/visnup/71ab08f7e707ebf9953db5d402cc290e/raw/0b3a42416e4b4687de983d3644cc5be80e286e90/SetCards.jpg" />
-        {/* <img ref={ref => this.video = ref} src="/static/SetCards.jpg" /> */}
+        <video ref={ref => this.video = ref} autoPlay muted playsInline />
         <canvas ref={ref => this.canvas = ref} />
       </div>
     )
