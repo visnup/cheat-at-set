d956506c979d91826a882fc2825e092e31bb68d5
Visnu Pitiyanuvath <visnup@gmail.com>
2019-01-23 10:02:15 -0800

    wrap micro lambdas in now-micro

--- api/debug/index.js
+++ api/debug/index.js
@@ -4,15 +4,18 @@ const getDb = require('./db')
 module.exports = async function(req) {
   const samples = (await getDb()).collection('samples')
 
-  if (req.method === 'POST') {
-    const body = await json(req)
-    const { ops } = await samples.insertOne(body)
-    return { _id: ops[0]._id, batch: ops[0].batch }
-  } else {
-    const sample = await samples
-      .find()
-      .sort({ _id: -1 })
-      .toArray()
-    return sample
+  switch (req.method) {
+    case 'POST':
+      const body = await json(req)
+      const { ops } = await samples.insertOne(body)
+      return { _id: ops[0]._id, batch: ops[0].batch }
+    case 'GET':
+      const sample = await samples
+        .find()
+        .sort({ _id: -1 })
+        .toArray()
+      return sample
+    default:
+      return null
   }
 }
