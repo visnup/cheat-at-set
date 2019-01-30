1920f9ae7a6b758dc14f4c9d59228922e0a3cd14
Visnu Pitiyanuvath <visnup@gmail.com>
2019-01-29 20:46:22 -0800

    shape detection improvements

--- next/lib/attributes/shape.js
+++ next/lib/attributes/shape.js
@@ -1,15 +1,28 @@
+import { mean } from 'd3-array'
 import { scaleThreshold } from 'd3-scale'
-import { polygonArea } from 'd3-polygon'
+import { polygonArea, polygonHull } from 'd3-polygon'
 
-const scale = scaleThreshold()
-  .domain([0.1, 0.13, 0.165, 0.2])
-  .range([null, 'diamond', 'squiggle', 'oval', null])
+const areaScale = scaleThreshold()
+  .domain([0.1, 0.15, 0.2])
+  .range([null, 'diamond-squiggle', 'oval', null])
 
 function shapeArea(contours, area) {
-  return polygonArea(contours[0]) / area
+  return mean(contours.map(contour => polygonArea(contour) / area))
+}
+
+const hullScale = scaleThreshold()
+  .domain([0.1, 0.2])
+  .range(['diamond', 'squiggle', null])
+
+function hullArea(contours) {
+  return mean(contours.map(contour => {
+    const hullArea = polygonArea(polygonHull(contour))
+    return (hullArea - polygonArea(contour)) / hullArea
+  }))
 }
 
 export default function shape(contours, width, height) {
   if (!contours.length) return null
-  return scale(shapeArea(contours, width * height))
+  const area = areaScale(shapeArea(contours, width * height))
+  return area === 'diamond-squiggle' ? hullScale(hullArea(contours)) : area
 }
